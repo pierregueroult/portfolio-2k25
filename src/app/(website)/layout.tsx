06559b587e8b263e58eac468674e485815737bb7
Pierre Guéroult <pierre.gueroult@univ-rouen.fr>
2024-12-21 16:57:15 +0100

    feat: add console "easter egg"

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -5,6 +5,7 @@ import "@/styles/globals.css";
 
 import TransitionProvider from "@/components/transition-provider";
 import KeyboardSounds from "@/components/keyboard-sounds";
+import EasterEggConsole from "@/components/easter-egg-console";
 
 export const metadata: Metadata = {};
 
@@ -15,6 +16,7 @@ export default function RootLayout({ children }: RootLayoutProps): ReactNode {
     <html lang="en" dir="ltr">
       <body className={`antialiased`} suppressHydrationWarning={true}>
         <KeyboardSounds />
+        <EasterEggConsole />
         <TransitionProvider>{children}</TransitionProvider>
       </body>
     </html>
