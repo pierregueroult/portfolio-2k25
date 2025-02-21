9e69de0bc67424da81ad5f5f0297dd6f0247e4d9
Pierre Guéroult <pierre.gueroult@univ-rouen.fr>
2025-02-21 11:30:40 +0100

    Add Tailwind CSS integration and global styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 import partytown from "@astrojs/partytown";
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: "https://pierregueroult.dev",
@@ -10,5 +11,8 @@ export default defineConfig({
     changefreq: "weekly",
     priority: 0.7,
     lastmod: new Date(),
-  }), partytown()]
+  }), partytown()],
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
