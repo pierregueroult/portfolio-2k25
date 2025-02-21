472ac139d86cebdc6bdd13b46715f6cfe902777b
Pierre Guéroult <pierre.gueroult@univ-rouen.fr>
2025-02-21 11:51:16 +0100

    Add ESLint configuration and validation for Astro, TypeScript, and JavaScript

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+import eslintPluginAstro from 'eslint-plugin-astro';
+import esLintTypescript from 'typescript-eslint';
+
+/** @type {import('eslint').Linter.Config} */
+const config = [
+    ...esLintTypescript.configs.recommended,
+    ...eslintPluginAstro.configs.recommended,
+];
+
+export default config;
