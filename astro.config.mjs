07301f3d173303288bcad395c766be46ec067bd8
Pierre Guéroult <pierre.gueroult@univ-rouen.fr>
2025-02-21 11:19:59 +0100

    Add Partytown integration and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
 
+import partytown from "@astrojs/partytown";
+
 export default defineConfig({
   site: "https://pierregueroult.dev",
   integrations: [sitemap({
     changefreq: "weekly",
     priority: 0.7,
     lastmod: new Date(),
-  })]
+  }), partytown()]
 });
