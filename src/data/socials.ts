b8e56f4de525f7593204285f97c3309509194c01
Pierre Guéroult <pierre.gueroult@univ-rouen.fr>
2025-02-26 17:14:10 +0100

    Add ListLayout and ProjectItem components, enhance WithTitleLayout, and update global styles for improved layout and presentation

---
+++ src/data/socials.ts
@@ -0,0 +1,36 @@
+export type Social = { title: string; link: string; description: string };
+
+export const socials: Social[] = [
+  {
+    title: "X (Twitter)",
+    link: "https://x.com/pierregueroult1",
+    description:
+      "I post and re-post meme about my work, and also some tech insights, and sometimes I share my blog posts.",
+  },
+  {
+    title: "Linkedin",
+    link: "https://www.linkedin.com/in/pierregueroult/",
+    description:
+      "So you can see my professional profile and connect with me. I re-post meme there too. That's the goal of Linkedin, right?",
+  },
+  {
+    title: "Github",
+    link: "https://github.com/pierregueroult",
+    description: "You can see my projects and contributions. Gimme a star if you like my work! (please)",
+  },
+  {
+    title: "Youtube",
+    link: "https://www.youtube.com/@pierregueroultdev",
+    description:
+      "I swear I will post some tech videos soon. I'm just waiting for the right moment and the right amount of time.",
+  },
+];
+
+export const featured: Social[] = [
+  {
+    title: "Selenite's Github Organization",
+    link: "https://github.com/selenite-live",
+    description:
+      "The organization we created for our school project - Selenite: Lost Contact. We are a team of 5 students working on a 3D game in Unity.",
+  },
+];
