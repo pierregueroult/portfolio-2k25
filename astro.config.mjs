be876c368fe00f940c68f34b2e3974929a33ab23
Pierre Guéroult <pierre.gueroult@univ-rouen.fr>
2025-03-05 22:00:29 +0100

    Release 3 project articles

--- astro.config.mjs
+++ astro.config.mjs
@@ -59,7 +59,6 @@ export default defineConfig({
   server: {
     headers: {
       "Content-Security-Policy":
-        "default-src 'self'; script-src 'self' https://use.typekit.net https://eu-assets.i.posthog.com https://www.googletagmanager.com https://www.google-analytics.com 'unsafe-inline' 'unsafe-eval'; script-src-elem 'self' https://www.googletagmanager.com https://www.google-analytics.com https://eu-assets.i.posthog.com 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' https://use.typekit.net https://p.typekit.net; object-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors 'none'; connect-src 'self' https://eu.i.posthog.com https://www.google-analytics.com https://www.googletagmanager.com https://region1.google-analytics.com; block-all-mixed-content; upgrade-insecure-requests;",
-    },
+"default-src 'self'; frame-src 'self' https://www.youtube.com; script-src 'self' https://use.typekit.net https://eu-assets.i.posthog.com https://www.googletagmanager.com https://www.google-analytics.com 'unsafe-inline' 'unsafe-eval'; script-src-elem 'self' https://www.googletagmanager.com https://www.google-analytics.com https://eu-assets.i.posthog.com 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' https://use.typekit.net https://p.typekit.net; object-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors 'none'; connect-src 'self' https://eu.i.posthog.com https://www.google-analytics.com https://www.googletagmanager.com https://region1.google-analytics.com; block-all-mixed-content; upgrade-insecure-requests;"    },
   },
 });
