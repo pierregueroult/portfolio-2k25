58bc2f291e197de566fd91cacc0bbedf40041394
Pierre Guéroult <pierre.gueroult@univ-rouen.fr>
2025-03-17 13:02:49 +0100

    Update GitHub Actions workflow to include linting and building steps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,11 @@ import eslintPluginAstro from "eslint-plugin-astro";
 import esLintTypescript from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config} */
-const config = [...esLintTypescript.configs.recommended, ...eslintPluginAstro.configs.recommended];
+const config = [
+    { files: ["**/*.{mjs,ts,astro}"] },
+    { ignores: ["**/dist/*", ".astro/*"] },
+  ...esLintTypescript.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+];
 
 export default config;
