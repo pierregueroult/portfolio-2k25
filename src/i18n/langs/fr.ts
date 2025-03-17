58dc0f36e7eebc69e4bc5ba21f792765de709cee
Pierre Guéroult <pierre.gueroult@univ-rouen.fr>
2025-03-17 14:06:51 +0100

    Move translations into dedicated files

---
+++ src/i18n/langs/fr.ts
@@ -0,0 +1,102 @@
+import type { Texts } from "../index";
+
+export const fr: Texts= {
+  // components
+  // --- header
+  "components.header.lang": "Langue",
+  "components.header.theme": "Thème",
+  "components.header.paris": "Paris",
+  "components.header.france": "France",
+  // --- navigation
+  "components.navigation.home": "Accueil",
+  "components.navigation.socials": "Réseaux",
+  "components.navigation.projects": "Projets",
+  "components.navigation.blog": "Blog",
+  // --- theme
+  "components.theme.default": "Thèmes par défaut",
+  "components.theme.special": "Thèmes spéciaux",
+  "components.theme.search": "Rechercher un thème",
+  // --- lang
+  "components.lang.choose": "Changer de langue",
+  "components.lang.current": "actuelle",
+  // --- modal
+  "components.modal.close": "Fermer",
+  // --- items
+  "components.items.more": "Lire la suite",
+  "components.items.now": "en ce moment",
+  "components.items.from": "De",
+  "components.items.to": "à",
+  "components.items.about": "à propos de",
+  "components.items.work.looking": "À la recherche",
+  "components.items.work.near": "autour de",
+  "components.items.certification.certifiedBy": "Certifié par",
+  // --- noTranslation
+  "components.noTranslation.title": "Traduction en cours...",
+  "components.noTranslation.text":
+    "Cette page n'est pas encore disponible en français, mais j'y travaille ! En attendant, vous pouvez la lire en anglais ou revenir plus tard pour la version traduite.",
+  // --- contact form
+  "components.contact.name": "Nom",
+  "components.contact.email": "Email",
+  "components.contact.message": "Écrivez votre message ici...",
+  "components.contact.send": "Envoyer",
+  "components.contact.consent": "En soumettant ce formulaire, vous acceptez d'être contacté par email. Voir la ",
+  "components.contact.privacy": "politique de confidentialité",
+  "components.contact.consent2": "pour plus d'informations.",
+  // pages
+  // --- blog
+  "pages.blog.meta.title": "Blog",
+  "pages.blog.meta.description":
+    "Lisez mes derniers articles, pensées et tutoriels techniques. J'écris sur le développement web, l'ingénierie logicielle, l'automatisation et la culture tech.",
+  "pages.blog.meta.keywords":
+    "blog, articles, pensées, tutoriels, développement web, ingénierie logicielle, automatisation, culture tech",
+  "pages.blog.title": "Blog - Réflexions & Expérimentations",
+  "pages.blog.subtitle":
+    "Explorer le développement web, l'automatisation et les nouvelles technologies—un projet à la fois.",
+  "pages.blog.articles": "Articles",
+  "pages.blog.translation.title": "Traduction en cours...",
+  "pages.blog.translation.text":
+    "Ces articles ne sont pas encore disponibles en français, mais j'y travaille ! En attendant, vous pouvez les lire en anglais ou revenir plus tard pour la version traduite.",
+  // --- socials
+  "pages.socials.meta.title": "Connectons nous",
+  "pages.socials.meta.description":
+    "Trouvez et suivez-moi sur les réseaux sociaux et les réseaux professionnels. Restez informé de mes derniers projets, articles de blog et insights tech.",
+  "page.socials.meta.keywords":
+    "réseaux, réseaux sociaux, réseaux professionnels, projets, articles de blog, insights tech",
+  "pages.socials.title": "Mes Réseaux - Restons Connectés",
+  "pages.socials.subtitle":
+    "Retrouvez-moi sur les plateformes sociales et professionnelles pour suivre mes projets et échanger autour du développement web et de l'automatisation.",
+  "pages.socials.featured": "Mis en avant",
+  "pages.socials.links": "Liens de mes réseaux",
+  "pages.socials.contact.title": "M'envoyer un message",
+  "pages.socials.contact.description":
+    "Vous avez une question, un projet ou juste envie de discuter, n'hésitez pas à me contacter !",
+  // --- projects
+  "pages.projects.meta.title": "Projets & Travaux",
+  "pages.projects.meta.description":
+    "Explorez mes derniers projets de développement web, outils d'automatisation et expérimentations tech. Toujours en construction, toujours en apprentissage.",
+  "pages.projects.meta.keywords":
+    "projets, travaux, développement web, outils d'automatisation, expérimentations tech",
+  "pages.projects.title": "Projets, Créations et contributions",
+  "pages.projects.subtitle":
+    "Voici quelques-uns des projets que j'ai menés, construits ou auxquels j'ai contribué. Je me concentre sur la création d'outils pour développeurs, de solutions d'automatisation et d'applications web innovantes qui améliorent la vie et repoussent les limites de la technologie.",
+  "pages.projects.creations": "Créations",
+  "pages.projects.projects": "Projets",
+  "pages.projects.contributions": "Contributions",
+  "pages.projects.translation.title": "Traduction en cours...",
+  "pages.projects.translation.text":
+    "Ces pages de projets ne sont pas encore disponible en français, mais j'y travaille ! En attendant, vous pouvez les lire en anglais ou revenir plus tard pour la version traduite.",
+  "pages.projects.schools": "Projets Scolaires",
+  // --- home (index)
+  "pages.index.meta.title": "Développeur Web & Logiciel",
+  "pages.index.meta.description":
+    "Développeur Web de 20 ans & Étudiant en Informatique passionné par la création de sites web élégants et performants et l'automatisation des workflows. Toujours à la recherche de nouvelles technologies pour résoudre des problèmes et optimiser l'efficacité.",
+  "pages.index.meta.keywords":
+    "développeur web, développeur logiciel, étudiant en informatique, développement web, automatisation, nouvelles technologies",
+  "pages.index.title": "Ingénieur Logiciel Full-Stack - Pierre Guéroult - Paris, France",
+  "pages.index.subtitle":
+    "Développeur web de 20 ans & Étudiant en Informatique. J'adore créer des sites web élégants et automatiser les workflows pour faciliter la vie. Passionné par le code propre, la résolution de problèmes et la mise en place de technologies intelligentes. Toujours à la recherche de nouvelles technologies, toujours prêt à relever un défi. Que ce soit le développement web, l'automatisation ou quelque chose complètement hors de ma zone de confort - je suis partant.",
+  "pages.index.works": "Emplois",
+  "pages.index.schools": "Écoles",
+  "pages.index.certifications": "Certifications",
+  "pages.index.curriculum": "Téléchargez mon Curriculum Vitae",
+};
