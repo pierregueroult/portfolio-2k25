295c0d5e14a66026120a9e1da17bd775c549418d
Pierre Guéroult <pierre.gueroult@univ-rouen.fr>
2025-03-24 15:50:13 +0100

    Update Content Security Policy to remove GA and GTM domains

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,7 +10,7 @@ export const onRequest = defineMiddleware(async (context, next) => {
 
   response.headers.set(
     "Content-Security-Policy",
-    "default-src 'self'; frame-src 'self' https://www.youtube.com; script-src 'self' https://use.typekit.net https://eu-assets.i.posthog.com https://www.googletagmanager.com https://www.google-analytics.com 'unsafe-inline' 'unsafe-eval'; script-src-elem 'self' https://www.googletagmanager.com https://www.google-analytics.com https://eu-assets.i.posthog.com 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://eu.posthog.com; img-src 'self' data: https:; font-src 'self' https://use.typekit.net https://p.typekit.net; object-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors 'none'; connect-src 'self' https://eu.i.posthog.com https://internal-t.posthog.com https://*.posthog.com https://www.google-analytics.com https://www.googletagmanager.com https://region1.google-analytics.com; block-all-mixed-content; upgrade-insecure-requests;",
+    "default-src 'self'; frame-src 'self' https://www.youtube.com; script-src 'self' https://use.typekit.net https://eu-assets.i.posthog.com 'unsafe-inline' 'unsafe-eval'; script-src-elem 'self' https://eu-assets.i.posthog.com 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://eu.posthog.com; img-src 'self' data: https:; font-src 'self' https://use.typekit.net https://p.typekit.net; object-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors 'none'; connect-src 'self' https://eu.i.posthog.com https://internal-t.posthog.com https://*.posthog.com; block-all-mixed-content; upgrade-insecure-requests;",
   );
   response.headers.set("Referrer-Policy", "strict-origin-when-cross-origin");
   response.headers.set("X-Content-Type-Options", "nosniff");
