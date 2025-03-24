b3891a70032af8fc8c0f2d4de4b625a9771a9e9f
Pierre Guéroult <pierre.gueroult@univ-rouen.fr>
2025-03-24 15:57:24 +0100

    Update middleware to enhance Permissions-Policy and remove deprecated security headers

--- src/middleware.ts
+++ src/middleware.ts
@@ -15,11 +15,7 @@ export const onRequest = defineMiddleware(async (context, next) => {
   response.headers.set("Referrer-Policy", "strict-origin-when-cross-origin");
   response.headers.set("X-Content-Type-Options", "nosniff");
   response.headers.set("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload");
-  response.headers.set("X-XSS-Protection", "1; mode=block");
-
-  response.headers.set("Permissions-Policy", "geolocation=(), microphone=(), camera=()");
-  response.headers.set("X-Frame-Options", "DENY");
-  response.headers.set("X-Permitted-Cross-Domain-Policies", "none");
+  response.headers.set("Permissions-Policy", "geolocation=(), microphone=(), camera=(), payment=(), usb=(), fullscreen=(self), display-capture=(), interest-cohort=()");
 
   return response;
 });
